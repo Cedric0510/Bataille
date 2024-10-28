8aa8a8be2a42a844dc1df658b4f2af2fc400a4d3
Cedric0510 <cedric.vanhove.82@gmail.com>
2024-10-28 11:25:40 +0100

    Deck added

--- Deck.ts
+++ Deck.ts
@@ -1,6 +1,9 @@
+import { Card } from "./card"
 export class Deck {
 
+    private card : Card[];
 
-
-    public shuffleCards()
+    public shuffleCards(card:Card):void{
+        // Mélangé les 52 cartes créé et distribuer.
+    }
 }
